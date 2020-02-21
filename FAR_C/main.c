9ca06f76f4a096e078e696d98cb2cba7c954e5f2
Baltic_Racing_ELEC_1 <racing@hochschule-stralsund.de>
2020-02-21 18:25:45 +0100

    initd repo

---
+++ FAR_C/main.c
@@ -0,0 +1,36 @@
+/*
+ * FAR_C.c
+ *
+ * Created: 21.02.2020 17:21:19
+ * Author : Admin
+ */ 
+
+#include <avr/io.h>
+#include "canlib.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
+// pa0 fan1fusein
+//pa1 fan2fusein
+//pa2 ignitionfusein
+//pa4 injectionfusein
+//pa5 fuelpumpfusein
+//pa6 controlelrfusein
+//pa7 backlight fuse in
+
+// pb5 emufusein
+//pb6 servo3fusein
+//pb7 servo2fusein
+//pg3 servo1fusein
+//pg4 dcdcinput fuse
+
+//pb0 fuelpump ctrl
+//Fan1_PWM
+//fan2pwm
